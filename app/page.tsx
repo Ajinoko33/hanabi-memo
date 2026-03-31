14abdad6fa32d35250dfe7ad975e8098b902c91d
Ajinoko <86123319+Ajinoko33@users.noreply.github.com>
2026-04-01 03:07:36 +0900

    feature: カードインデックスを sticky にする (#42)

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Button, Col, Divider, Modal, Row } from 'antd'
 import { ActionRow } from './_components/ActionRow'
 import { ActionForm } from './_components/form/ActionForm'
 import { useActionLog } from './_hooks/useActionLog'
-import { useCheckIsStale } from './_hooks/useCheckIstale'
+import { useCheckIsStale } from './_hooks/useCheckIsStale'
 
 const titles = [CARDS[1], CARDS[2], CARDS[3], CARDS[4], CARDS[5]].map(
   (card) => card.label,
@@ -19,22 +19,24 @@ export default function Home() {
   const { isOpen, open, handleOk, handleCancel } = useModalManipulation(clear)
 
   return (
-    <main className='flex flex-col items-center flex-1 p-4 bg-white'>
-      <div className='w-96 border-r'>
+    <main className='flex flex-col items-center flex-1 px-4 pb-4 bg-white'>
+      <div className='w-96'>
         {/* title */}
-        <Row className='w-full'>
-          {titles.map((value) => {
-            return (
-              <Col
-                key={value}
-                flex={1}
-                className='border-t border-b-2 border-l'
-              >
-                <div className='flex justify-center'>{value}</div>
-              </Col>
-            )
-          })}
-        </Row>
+        <div className='w-full sticky top-0 pt-4 bg-white z-10'>
+          <Row className='w-full border-r'>
+            {titles.map((value) => {
+              return (
+                <Col
+                  key={value}
+                  flex={1}
+                  className='border-t border-b-2 border-l'
+                >
+                  <div className='flex justify-center'>{value}</div>
+                </Col>
+              )
+            })}
+          </Row>
+        </div>
 
         {/* content */}
         {logs.map((action) => {
