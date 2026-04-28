d16792b9445f3925d9bf891e0762a9d46df266f6
Ajinoko33 <2022kyopr03@gmail.com>
2026-04-29 01:05:45 +0900

    実験

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,86 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Hanabi Memo',
   description: 'ボードゲーム「花火」のメモ用アプリ',
+  appleWebApp: {
+    capable: true,
+    startupImage: [
+      {
+        url: 'icons/splash/icon-320x480.png',
+        media:
+          '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-640x960.png',
+        media:
+          '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-640x1136.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-750x1334.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-828x1792.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1125x2436.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1170x2532.png',
+        media:
+          '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1179x2556.png',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1206x2622.png',
+        media:
+          '(device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1080x1920.png',
+        media:
+          '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-828x1792.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1260x2736.png',
+        media:
+          '(device-width: 420px) and (device-height: 912px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1284x2778.png',
+        media:
+          '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1290x2796.png',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: 'icons/splash/icon-1320x2868.png',
+        media:
+          '(device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
